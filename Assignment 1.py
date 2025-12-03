6ecc44379b2214f9346023a78f7b5f09775a64ca
Rachita292007 <dukarerachita@gmail.com>
2025-12-03 23:43:45 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,22 @@
+a=input("Enter the First Number : ")
+b=input("Enter the Second Number : ")
+c=float(a)
+d=float(b)
+
+# Addition
+add=c+d
+print("The sum is",add)
+
+# subtraction
+sub=c-d
+print("The subtraction is",sub)
+
+#Multiplication
+mul=c*d
+print("The multiplication is",mul)
+
+# Division
+div=c/d
+print("The division is",div)
+
+
